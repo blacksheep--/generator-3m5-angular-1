ca062ccb1b5186faf59a31dc1cd0e40173b02277
3m5-rizovss <igvadaimon@gmail.com>
2016-11-29 10:00:17 +0100

    event management example

--- generators/app/templates/src/app/components/pages/dashboard/events/EventsExample.component.js
+++ generators/app/templates/src/app/components/pages/dashboard/events/EventsExample.component.js
@@ -1,18 +1,41 @@
 import angular from 'angular';
 
+/*
+ !!!!!!!!!!!
+ Angular has an event system, but it SHOULD BE USED WITH CAUTION
+ Only use this if ALL OTHER methods of communication between components do not work
+ !!!!!!!!!!!
+ */
+
 export const EventsExample = {
 	template: require('./eventsExample.template.html'),
 	controller: class {
-		constructor($rootScope) {
-			angular.extend(this, {$rootScope});
-			/*
-			 Angular has an event system, but it should not be overused
-			 Only use this if all other methods of communication between components do not work
-			 */
+		constructor($scope, $rootScope) {
+			angular.extend(this, {$scope, $rootScope});
 		}
 
 		onAppLevelEventClicked() {
+			/*
+			 $rootScope.$emit only lets other $rootScope listeners catch it.
+			 This is good when you don't want every $scope to get it. Mostly a high level communication.
+			 Listener is in AppHeader
+			 */
 			this.$rootScope.$emit('appLevelEvent', {someData: 123});
+		}
+
+		onScopeBroadcastClicked() {
+			/*
+			 $scope.$broadcast is for the $scope itself and its children.
+			 This is a child whispering to its stuffed animals so their parents can't hear.
+			 */
+			this.$scope.$broadcast('toChildrenEvent');
+		}
+
+		onScopeEmitClicked() {
+			/*
+			 $scope.$emit is when you want that $scope and all its parents and $rootScope to hear the event.
+			 */
+			this.$scope.$emit('toParentsEvent');
 		}
 	}
 };
